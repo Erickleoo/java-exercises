d80d1a8f94881fb0e2a77a379b23b2efcfef44f2
ErickLeoo <ericleonardo_2012@hotmail.com>
2025-09-22 21:52:56 -0300

    constructors, this whord, overloading and encapsulation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -11,14 +11,12 @@ public class Program2 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
         System.out.println("Enter product data:");
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
-        System.out.print("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        double price = sc.nextDouble();
+        Product product = new Product(name, price);
 
         System.out.println();
         System.out.println("Product data: " + product);
@@ -29,7 +27,7 @@ public class Program2 {
         product.addProducts(quantity);
 
         System.out.println();
-        System.out.println("Updated data: " + product);
+        System.out.println("Updated data: " + product.getName());
 
         System.out.println();
         System.out.print("Enter the number of products to be removed from stock: ");
